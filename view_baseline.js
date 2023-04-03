7b1c2e085fae9e8afe3d62c471b86926825359c6
Alice Mao <amao@wustl.edu>
2023-04-02 21:44:02 -0500

    add instructions

--- view_baseline.js
+++ view_baseline.js
@@ -145,7 +145,7 @@ perm_remove_user_button.click(function(){
 
 // --- Append all the elements to the permissions dialog in the right order: --- 
 perm_dialog.append(obj_name_div)
-perm_dialog.append($('<div id="permissions_user_title">Group or user names:</div>'))
+perm_dialog.append($('<br><div id="permissions_user_title">Select a Group or user name to add, remove, and to view their permissions<br><br></div>'))
 perm_dialog.append(file_permission_users)
 perm_dialog.append(perm_add_user_select)
 perm_add_user_select.append(perm_remove_user_button) // Cheating a bit again - add the remove button the the 'add user select' div, just so it shows up on the same line.
