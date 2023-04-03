886b0466a15f9fe4e36ae5002ee8623bf82892e0
Alice Mao <amao@wustl.edu>
2023-04-03 18:33:13 -0500

    delete duplicate function

--- view.js
+++ view.js
@@ -110,7 +110,7 @@ $('.permbutton').click(function (e) {
 
     // open_advanced_dialog(path)
     perm_dialog.dialog('open')
-    // open_permissions_dialog(path)
+    open_permissions_dialog(path)
 
   // Deal with the fact that folders try to collapse/expand when you click on their permissions button:
   e.stopPropagation(); // don't propagate button click to element underneath it (e.g. folder accordion)
@@ -163,41 +163,3 @@ $("body").on("input", function () {
 //     $("#helpBody").css("display", "block");
 // });
 
-
-
-
-
-
-
-function print_files(file_obj) {
-  let file_hash = get_full_path(file_obj);
-
-  if (file_obj.is_folder) {
-    let folder_elem = $(`<div class='folder' id="${file_hash}_div">
-            <h3 id="${file_hash}_header">
-                <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
-                <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/>  Permissions
-                </button>
-            </h3>
-        </div>`);
-
-    // append children, if any:
-    if (file_hash in parent_to_children) {
-      let container_elem = $("<div class='folder_contents'></div>");
-      folder_elem.append(container_elem);
-      for (child_file of parent_to_children[file_hash]) {
-        let child_elem = make_file_element(child_file);
-        container_elem.append(child_elem);
-      }
-    }
-    return folder_elem;
-  } else {
-    return $(`<div class='file'  id="${file_hash}_div">
-            <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
-            <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/>  Permissions
-            </button>
-        </div>`);
-  }
-}
